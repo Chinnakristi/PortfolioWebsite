cf6578d8ac15cf8100c8561ed5c0566a1b70c130
Chinnakristi <https://github.com/Chinnakristi>
2026-09-12 17:34:15 +0530

    Initial commit - PortfolioWebsite project

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if (name === "" || email === "" || message === "") {
+    alert("All fields must be filled out!");
+    return false;
+  }
+  alert("Message sent successfully!");
+  return true;
+}
